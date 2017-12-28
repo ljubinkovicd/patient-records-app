4542b814197ad02d0ecea0a20dbca49f2acd4228
Dorde Ljubinkovic <ljubinkovic.dorde@gmail.com>
2017-12-28 21:49:00 +0100

    Initial Commit

---
+++ InterviewProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InterviewProject
+//
+//  Created by Dorde Ljubinkovic on 12/28/17.
+//  Copyright © 2017 Dorde Ljubinkovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
